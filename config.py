e26b408e1e403f679536e7d7cc84985c4f835e80
user <ermolin.andrei2017@yandex.ru>
2023-02-26 06:13:28 +0300

    Initial&last commit(old project)

---
+++ config.py
@@ -0,0 +1,7 @@
+# основа бота Irregulariti
+settings = {
+    'token': 'ODE0NTIwNjYzNzgzMTEyNzM2.YDfDew.yFd17rmUlWf0ZpXSeCTn27RIjsI',
+    'bot': 'Irregulariti',
+    'id': 814520663783112736,
+    'prefix': '-'
+}
